f0d7fe7794ef9faabae4d3a65bf41200532a6ea3
Mariia Bilohan <mariiabilohan@Mariias-MacBook-Air.local>
2023-06-09 16:48:02 +0300

    bulls and cows game

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,205 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+  import { ref, onMounted } from 'vue'
+  import type { Ref } from 'vue'
+
+  import GameRules from './components/GameRules.vue'
+
+  type Attempt = {
+    number: number[],
+    bulls: number,
+    cows: number
+  }
+
+  let secretNumber: Ref<number[]> = ref([])
+
+  let newGuess: Ref<number[]> = ref([])
+
+  function addNumber (number: number) {
+    newGuess.value.push(number)
+  }
+
+  function removeNumber () {
+    newGuess.value.pop()
+  }
+
+  let attempts: Ref<Attempt[]> = ref([])
+
+  let isWinner: Ref<boolean> = ref(false)
+
+  onMounted(() => {
+    startNewGame()
+
+    window.addEventListener("keydown", (e) => {
+      if (e.key === 'Enter') {
+        e.preventDefault()
+
+        if (isWinner.value) {
+          startNewGame()
+        } else if (newGuess.value.length === 4) {
+          addGuess()
+        }
+
+        return
+      }
+
+      if(e.key === 'Backspace') {
+        removeNumber()
+
+        return
+      }
+
+      const key = +e.key
+      if (key >= 0 && key <= 9) { 
+        addNumber(key)
+      }
+    })
+  })
+
+  function startNewGame () {
+    const newNumber: Set<number> = new Set()
+    while (newNumber.size < 4) {
+      const digit = Math.floor(0 + Math.random() * 9)
+      newNumber.add(digit)
+    }
+    
+    secretNumber.value = Array.from(newNumber)
+
+    newGuess.value = []
+    attempts.value = []
+    isWinner.value = false
+  }
+
+  function addGuess () {
+    let cows = 0
+    let bulls = 0
+
+    newGuess.value.forEach((n, index) => {
+      if (secretNumber.value.includes(n)) {
+        
+        if (secretNumber.value[index] === n) {
+          bulls +=1
+        } else {
+          cows += 1
+        }
+      }
+    });
+
+    attempts.value.push({
+      number: [...newGuess.value],
+      cows,
+      bulls
+    })
+
+    newGuess.value = []
+
+    // check winner
+    if (bulls === 4) {
+      isWinner.value = true
+    }
+  }
+
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <div class="container mx-auto py-8 px-4 text-center">
+    <h1 class="text-3xl mb-4">Try to guess</h1>
+    <p class="mb-6">
+      <button 
+        class="bg-green-700 hover:bg-green-800 border-b-4 border-green-800 hover:border-green-900 text-xl tracking-wider text-white font-bold py-4 px-6 rounded"
+        type="button" 
+        @click="startNewGame"
+      >New game
+      </button>
+    </p>
+    <h2
+      v-show="isWinner"
+      class="text-green-600 font-bold text-2xl my-4"
+    >
+      You win!!!
+    </h2>
+    <div class="relative overflow-x-auto mb-6 max-w-5xl mx-auto">
+      <table class="border-collapse	border-spacing-0 table-auto w-full">
+        <thead>
+          <tr>
+            <th
+              class="border p-2"
+            >#</th>
+            <th
+              class="border p-2"
+            >Your guess</th>
+            <th
+              class="border p-2"
+            >Cows</th>
+            <th
+              class="border p-2"
+            >Bulls</th>
+          </tr>
+        </thead>
+        <tbody v-if="attempts.length">
+          <tr v-for="(attempt, i) in attempts">
+            <td 
+              class="border p-2"
+            >
+              {{ i + 1 }}
+            </td>
+            <td 
+              class="border p-2"
+            >
+              {{ attempt.number.join('') }}
+            </td>
+            <td 
+              class="border p-2"
+            >
+              {{ attempt.cows }}
+            </td>
+            <td 
+              class="border p-2"
+            >
+              {{ attempt.bulls }}
+            </td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+    <form>
+      <h2 class="font-bold mb-4 text-2xl">
+        Your number 
+        <span v-for="number in newGuess">{{ number }}</span>
+      </h2>
+      <div class="max-md:flex max-md:justify-center max-md:align-center">
+        <p class="mb-4 md:space-x-0.5 max-md:order-1 max-md:space-y-0.5">
+          <button 
+            type="submit" 
+            class="bg-green-700 hover:bg-green-800 disabled:bg-gray-500 text-white font-bold border border-green-700 disabled:border-gray-400 py-2 px-4 rounded max-md:w-full" 
+            :disabled="isWinner || newGuess.length !== 4"
+            @click.prevent="addGuess"
+          >GO</button>
+          <button
+            class="bg-white hover:bg-gray-100 disabled:bg-gray-500 text-gray-800 disabled:text-white font-semibold py-2 px-4 border border-gray-400 rounded max-md:w-full"
+            type="button"
+            :disabled="isWinner || !newGuess.length"
+            @click="removeNumber"
+          >
+            Remove
+          </button>
+        </p>
+        <p class="space-x-0.5 space-y-0.5">
+          <button 
+            v-for="i in 10"
+            :key="i"
+            class="bg-sky-600 hover:bg-sky-800 disabled:bg-gray-500 text-white font-bold py-2 px-4 rounded max-md:w-1/6"
+            :disabled="isWinner || newGuess.length >= 4 || newGuess.includes(i - 1)"
+            type="button"
+            @click="addNumber(i-1)"
+          >
+              {{ i - 1 }}
+          </button>
+        </p>
+      </div>
+    </form>
+
+    <game-rules />
+    
+  </div>
+</template>
---
+++ src/components/GameRules.vue
@@ -0,0 +1,38 @@
+<template>
+  <button
+    class="absolute top-2 right-2 p-1 border border-gray-600 rounded"
+    title="Show rules"
+    @click="isRulesVisible = !isRulesVisible"
+  >
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+      <path stroke-linecap="round" stroke-linejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z" />
+    </svg>
+  </button>
+
+  <div 
+    v-show="isRulesVisible" 
+    class="fixed top-0 left-0 min-h-full min-w-full flex justify-center items-center"
+  >
+    <div 
+      class="fixed top-0 left-0 min-h-full min-w-full bg-gray-400/50"
+      @click="isRulesVisible = false"
+    ></div>
+    <div class="overflow-y-auto max-h-screen w-full p-4">
+      <div class="bg-white max-w-lg w-full m-auto px-4 pb-4 pt-5 sm:p-6 sm:pb-4 space-y-4 shadow-lg shadow-gray-500 rounded z-5 relative text-left">
+        <h4 class="text-xl text-center font-bold">How to play</h4>
+        <p>Bulls and Cows is a 2 player game. One player thinks of a number, while the other player tries to guess it.</p>
+        <ul class="list-disc list-inside space-y-2">
+          <li>The number to be guessed must be a 4 digit number, using only digits from 1 - 9, each digit atmost once. e.g. 1234 is valid, 0123 is not valid, 9877 is not valid, 9876 is valid.</li>
+          <li>For every guess that the player makes, he gets 2 values - the number of bulls and the number of cows. 1 bull means the guess contains and the target number have 1 digit in common, and in the correct position. 1 cow means the guess and the target have 1 digit in common, but not in correct position. e.g. Let the target be 1234. Guessing 4321 will give 0 bulls and 4 cows. 3241 will give 1 bull and 3 cows. 4 bulls means you have won the game!</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Ref } from 'vue'
+
+const isRulesVisible: Ref<boolean> = ref(false)
+</script>
